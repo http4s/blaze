476fe9898e6fff6ab7c9a2b9688fd49a192e71c4
Bryce Anderson <bryce.anderson22@gmail.com>
2014-01-17 12:24:35 -0500

    Move transcoder code to java. It was basically java anyways. More tests, all passing.

--- src/main/scala/blaze/pipeline/stages/http/websocket/WebSocketDecoder.scala
+++ src/main/scala/blaze/pipeline/stages/http/websocket/WebSocketDecoder.scala
@@ -4,14 +4,16 @@ import blaze.pipeline.stages.ByteToObjectStage
 import java.nio.ByteBuffer
 
 import WebsocketBits._
-import java.nio.charset.StandardCharsets
 import scala.util.Random
+import blaze.pipeline.stages.http.websocket.WebSocketDecoder.WebSocketFrame
+import blaze.pipeline.stages.http.websocket.FrameTranscoder.TranscodeError
 
 /**
  * @author Bryce Anderson
  *         Created on 1/15/14
  */
-class WebSocketDecoder(isClient: Boolean, val maxBufferSize: Int = 0) extends ByteToObjectStage[WebSocMessage] {
+class WebSocketDecoder(isClient: Boolean, val maxBufferSize: Int = 0)
+      extends FrameTranscoder(isClient) with ByteToObjectStage[WebSocketFrame] {
 
   val name = "Websocket Decoder"
 
@@ -19,57 +21,10 @@ class WebSocketDecoder(isClient: Boolean, val maxBufferSize: Int = 0) extends By
     * @param in object to decode
     * @return sequence of ByteBuffers to pass to the head
     */
-  def messageToBuffer(in: WebSocMessage): Seq[ByteBuffer] = {
-    val size = { 2 +
-      (if (isClient) 4 else 0) +
-      (if (in.length < 126) 0 else if (in.length <= 0xffff) 2 else 8) +
-      in.length
-    }
-    val buff = ByteBuffer.allocate(size)
+  @throws[TranscodeError]
+  def messageToBuffer(in: WebSocketFrame): Seq[ByteBuffer] = {
 
-    val opcode = in match {
-      case _: Continuation  => CONTINUATION
-      case _: BinaryMessage => BINARY
-      case _: Ping          => PING
-      case _: Pong          => PONG
-      case _: Close         => CLOSE
-      case _: TextMessage   => TEXT
-    }
-    
-    if (opcode == PING && in.length > 125) transcodeError("Invalid PING frame: frame too long.")
-    
-    // OP Code
-    buff.put(((if (in.finished) FINISHED else 0x0) | opcode).toByte)
-
-    buff.put{((if (isClient) MASK else 0x0) |
-      (if (in.length < 126) in.length
-       else if (in.length <= 0xffff) 126
-       else 127)
-      ).toByte }
-
-    // Put the length if it is not already set
-    if (in.length > 125 && in.length <= 0xffff) {
-      buff.put(((in.length >> 8) & 0xff).toByte)
-        .put((in.length & 0xff).toByte)
-    } else if (in.length > 0xffff) buff.putLong(in.length)
-
-    if (isClient) {
-      val mask = Random.nextInt()
-      val maskBits = Array(((mask >> 24) & 0xff).toByte,
-                           ((mask >> 16) & 0xff).toByte,
-                           ((mask >>  8) & 0xff).toByte,
-                           ((mask >>  0) & 0xff).toByte)
-
-      buff.put(maskBits)
-
-      var i = 0
-      while (i < in.length) {
-        buff.put((in.data(i) ^ maskBits(i & 0x3)).toByte) // i & 0x3 is the same as i % 4 but faster
-        i += 1
-      }
-    } else buff.put(in.data)
-
-    buff.flip()
+    val buff = _messageToBuffer(in)
     buff::Nil
   }
 
@@ -78,108 +33,36 @@ class WebSocketDecoder(isClient: Boolean, val maxBufferSize: Int = 0) extends By
     * @param in ByteBuffer of immediately available data
     * @return optional message if enough data was available
     */
-  def bufferToMessage(in: ByteBuffer): Option[WebSocMessage] = {
+  @throws[TranscodeError]
+  def bufferToMessage(in: ByteBuffer): Option[WebSocketFrame] = Option(_bufferToMessage(in))
+}
 
-    if (in.remaining() < 2) return None
+object WebSocketDecoder {
+  case class WebSocketFrame(opcode: Int, data: Array[Byte], finished: Boolean) {
+    def length = data.length
 
-    val len = getMsgLength(in)
+    override def equals(obj: Any): Boolean = obj match {
+      case WebSocketFrame(opcode, data, finished) =>
+        this.opcode == opcode &&
+        this.finished == finished &&
+        this.data.length == data.length && {
+          var i = 0
+          var same = true
+          while (i < data.length && same) {
+            if (data(i) != this.data(i)) same = false
+            i += 1
+          }
+          same
+        }
 
-    if (len < 0) return None
-
-    val opcode = in.get(0) & OP_CODE
-    val finished = (in.get(0) & FINISHED) != 0
-    val masked = (in.get(1) & MASK) != 0
-
-    if (masked && isClient) transcodeError("Client received a masked message")
-    val m = if (masked) getMask(in) else null
-    
-    val bodyOffset = lengthOffset(in) + (if (masked) 4 else 0)
-    val oldLim = in.limit()
-    val bodylen = bodyLength(in)
-
-    in.position(bodyOffset)
-    in.limit(in.position() + bodylen)
-
-    val slice = in.slice()
-
-    in.position(in.limit())
-    in.limit(oldLim)
-
-    val result = opcode match {
-    case CONTINUATION => Continuation(decodeBinary(slice, m), finished)
-    case TEXT =>  TextMessage(decodeBinary(slice, m), finished)
-    case BINARY => BinaryMessage(decodeBinary(slice, m), finished)
-    case CLOSE => Close(decodeBinary(slice, m), finished)
-    case PING => Ping(decodeBinary(slice, m), finished)
-    case PONG => Pong(decodeBinary(slice, m), finished)
-    case _ => transcodeError(s"Unknown message type: " + Integer.toHexString(opcode))
-
+      case _ => false
     }
-    Some(result)
-  }
 
-  private def transcodeError(msg: String): Nothing = sys.error(msg)
-
-  private def decodeBinary(in: ByteBuffer, mask: Array[Byte]): Array[Byte] = {
-
-    val data = new Array[Byte](in.remaining())
-    in.get(data)
-
-    if (mask != null) {  // We can use the charset decode
-      var i = 0
-      while (i < data.length) {
-        data(i) = (data(i) ^ mask(i & 0x3)).toByte   // i mod 4 is the same as i & 0x3 but slower
-        i += 1
-      }
+    override def toString: String = {
+      "WebSocketFrame(" +
+        Integer.toHexString(opcode) + ", Bytes(" +
+        data.length + "), finished: " + finished + ")"
     }
-    data
-  }
-
-  private def lengthOffset(in: ByteBuffer): Int = {
-    val len = (in.get(1) & LENGTH) >> 1
-
-    val offset = if (len < 126) 2
-    else if (len == 126) 4
-    else if (len == 127) 10
-    else transcodeError("Length error!")
-
-    offset
-  }
-
-  private def getMask(in: ByteBuffer): Array[Byte] = {
-    val m = new Array[Byte](4)
-    in.mark()
-    in.position(lengthOffset(in))
-    in.get(m)
-    in.reset()
-    m
-  }
-
-  private def bodyLength(in: ByteBuffer): Int = {
-    val len = (in.get(1) & LENGTH)
-    if (len < 126) len
-    else if (len == 126) in.getShort(2)
-    else if (len == 127) {
-      val l = in.getLong(2)
-      if (l > Int.MaxValue) transcodeError("Frame is too long")
-      else l.toInt
-    }
-    else transcodeError("Length error")
-  }
-
-  private def getMsgLength(in: ByteBuffer): Int = {
-    var totalLen = if ((in.get(1) & MASK) != 0) 6 else 2
-    val len = in.get(1) & LENGTH
-
-    if (len == 126) totalLen += 2
-    if (len == 127) totalLen += 8
-
-    if (in.remaining() < totalLen) return -1
-
-    totalLen += bodyLength(in)
-
-    if (in.remaining() < totalLen) -1
-    else totalLen
   }
 }
 
--- src/test/scala/blaze/pipeline/stages/http/websocket/WebsocketSpec.scala
+++ src/test/scala/blaze/pipeline/stages/http/websocket/WebsocketSpec.scala
@@ -9,6 +9,8 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 import java.nio.charset.StandardCharsets.UTF_8
+import blaze.pipeline.stages.http.websocket.WebSocketDecoder.WebSocketFrame
+import scala.util.Random
 
 /**
  * @author Bryce Anderson
@@ -22,9 +24,9 @@ class WebsocketSpec extends WordSpec with Matchers {
 
   def helloTxt = Array(0x81, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f).map(_.toByte)
 
-  class Reporter extends TailStage[WebSocMessage] {
+  class Reporter extends TailStage[WebSocketFrame] {
     def name: String = "Fetcher"
-    def getMessage(): WebSocMessage = {
+    def getMessage(): WebSocketFrame = {
       Await.result(channelRead(), 4.seconds)
     }
   }
@@ -41,17 +43,32 @@ class WebsocketSpec extends WordSpec with Matchers {
     tail
   }
 
-  def decode(msg: Array[Byte], isClient: Boolean): WebSocMessage = {
+  def decode(msg: Array[Byte], isClient: Boolean): WebSocketFrame = {
     val buff = ByteBuffer.wrap(msg)
 
     new WebSocketDecoder(isClient).bufferToMessage(buff).get
   }
 
-  def encode(msg: WebSocMessage, isClient: Boolean): Array[Byte] = {
+  def encode(msg: WebSocketFrame, isClient: Boolean): Array[Byte] = {
     new WebSocketDecoder(isClient).messageToBuffer(msg).head.array()
   }
 
-    "Websocket decoder" should {
+  "Websocket decoder" should {
+
+    "equate frames correctly" in {
+      val f1 = WebSocketFrame(WebsocketBits.BINARY, Array(0x2.toByte, 0x3.toByte), true)
+      val f11 = WebSocketFrame(WebsocketBits.BINARY, Array(0x2.toByte, 0x3.toByte), true)
+      val f2 = WebSocketFrame(WebsocketBits.BINARY, Array(0x2.toByte, 0x3.toByte), false)
+      val f3 = WebSocketFrame(WebsocketBits.TEXT, Array(0x2.toByte, 0x3.toByte), true)
+      val f4 = WebSocketFrame(WebsocketBits.BINARY, Array(0x2.toByte, 0x4.toByte), true)
+
+      f1 should equal(f1)
+      f1 should equal(f11)
+      f1 should not equal(f2)
+      f1 should not equal(f3)
+      f1 should not equal(f4)
+    }
+
     "decode a hello world message" in {
 //      val p = pipeline(helloTxtMasked, false)
 //      p.getMessage() should equal (TextMessage("Hello".getBytes(UTF_8), true))
@@ -70,9 +87,34 @@ class WebsocketSpec extends WordSpec with Matchers {
     }
 
     "encode a hello world message" in {
-      val msg = decode(encode(TextMessage("Hello".getBytes(UTF_8), false), true), false)
+      val frame = WebSocketFrame(WebsocketBits.TEXT, "Hello".getBytes(UTF_8), false)
+      val msg = decode(encode(frame, true), false)
+      msg should equal (frame)
       msg.finished should equal (false)
       new String(msg.data, UTF_8) should equal ("Hello")
+    }
+
+    "encode and decode a message with 125 < len <= 0xffff" in {
+      val bytes = 0 until 0xfff map(_.toByte) toArray
+      val frame = WebSocketFrame(WebsocketBits.BINARY, bytes, false)
+
+      val msg = decode(encode(frame, true), false)
+      val msg2 = decode(encode(frame, false), true)
+
+      msg should equal(frame)
+      msg should equal(msg2)
+
+    }
+
+    "encode and decode a message len > 0xffff" in {
+      val bytes = 0 until (0xffff + 1) map(_.toByte) toArray
+      val frame = WebSocketFrame(WebsocketBits.BINARY, bytes, false)
+
+      val msg = decode(encode(frame, true), false)
+      val msg2 = decode(encode(frame, false), true)
+
+      msg should equal(frame)
+      msg should equal(msg2)
     }
   }
 
