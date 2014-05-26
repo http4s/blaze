760ca2dfe6f3c5e5b2e57514530a1bce3bd193cd
Bryce Anderson <bryce.anderson22@gmail.com>
2014-05-26 08:48:32 -0400

    Removed unused method on ServerChannelFactory

--- core/src/main/scala/org/http4s/blaze/channel/ServerChannelFactory.scala
+++ core/src/main/scala/org/http4s/blaze/channel/ServerChannelFactory.scala
@@ -8,9 +8,5 @@ import java.net.SocketAddress
  *         Created on 1/23/14
  */
 trait ServerChannelFactory[C <: NetworkChannel] {
-
   def bind(localAddress: SocketAddress = null): ServerChannel
-
-  // Intended to be overridden in order to allow the reject of connections
-  protected def acceptConnection(channel: C): Boolean = true
 }
