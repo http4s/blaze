http4s/blaze

commit b679c87fc80992aefa4a54bacb6e7e532caa078a

Fixes issue with blaze client and the request line encoding.
Closes http4s/http4s#60. Thanks @diagonal-e for the bug report!

blaze-client/src/main/scala/org/http4s/client/blaze/Http1ClientStage.scala
@@ -96,7 +96,7 @@ class Http1ClientStage(protected val timeout: Duration = 60.seconds)
 
   private def encodeRequestLine(req: Request, writer: Writer): writer.type = {
     val uri = req.uri
-    writer ~ req.method ~ ' ' ~ uri.path ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
+    writer ~ req.method ~ ' ' ~ uri.toString ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
     if (getHttpMinor(req) == 1 && Host.from(req.headers).isEmpty) { // need to add the host header for HTTP/1.1
       uri.host match {
         case Some(host) =>

blaze-client/src/test/scala/org/http4s/client/blaze/Http1ClientStageSpec.scala
@@ -1,0 +1,48 @@
+package org.http4s
+package client.blaze
+
+import java.nio.charset.StandardCharsets
+
+import org.http4s.blaze.SeqTestHead
+import org.http4s.blaze.pipeline.LeafBuilder
+import org.specs2.mutable.Specification
+
+import java.nio.ByteBuffer
+
+import org.specs2.time.NoTimeConversions
+import scodec.bits.ByteVector
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+import scalaz.\/-
+
+// TODO: this needs more tests
+class Http1ClientStageSpec extends Specification with NoTimeConversions {
+
+  def getSubmission(req: Request, resp: String): String = {
+    val tail = new Http1ClientStage()
+    val h = new SeqTestHead(List(ByteBuffer.wrap(resp.getBytes(StandardCharsets.US_ASCII))))
+    LeafBuilder(tail).base(h)
+
+    val result = tail.runRequest(req).run
+    h.stageShutdown()
+    val buff = Await.result(h.result, 30.seconds)
+    new String(ByteVector(buff).toArray, StandardCharsets.US_ASCII)
+  }
+
+  "Http1ClientStage" should {
+    "Submit a request line with a query" in {
+      val resp = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\ndone"
+      val uri = "http://www.foo.com/huh?foo=bar"
+      val \/-(parsed) = Uri.fromString(uri)
+      val req = Request(uri = parsed)
+
+      val response = getSubmission(req, resp).split("\r\n")
+      val statusline = response(0)
+
+      statusline must_== "GET " + uri + " HTTP/1.1"
+    }
+  }
+
+}

blaze-client/src/test/scala/org/http4s/client/blaze/BlazeHttp1ClientSpec.scala
@@ -10,6 +10,7 @@ import org.specs2.time.NoTimeConversions
 
 import scala.concurrent.duration._
 
+// TODO: this should have a more comprehensive test suite
 class BlazeHttp1ClientSpec extends Http4sSpec with NoTimeConversions with After {
 
   "Blaze Simple Http1 Client" should {
@@ -38,7 +39,7 @@ class BlazeHttp1ClientSpec extends Http4sSpec with NoTimeConversions with After 
       val resp = Request(GET, uri("https://github.com/")).on(Status.Ok)(EntityDecoder.text).run
       //      println(resp.copy(body = halt))
 
-      resp.status.code must be_==(200)
+      resp.status.code must_==(200)
     }
 
     "Repeat a simple http request" in {

commit d4c456cb6ff1ae5b905f5fe719f5f267864b3e9d

Fix bug in blaze-client introduced by me in commit b679c87fc80992aefa4a54bacb6e7e532caa078a
Clearly the test coverage is lacking or this would have been caught.

blaze-client/src/main/scala/org/http4s/client/blaze/Http1ClientStage.scala
@@ -96,7 +96,7 @@ class Http1ClientStage(protected val timeout: Duration = 60.seconds)
 
   private def encodeRequestLine(req: Request, writer: Writer): writer.type = {
     val uri = req.uri
-    writer ~ req.method ~ ' ' ~ uri ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
+    writer ~ req.method ~ ' ' ~ uri.copy(scheme = None, authority = None) ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
     if (getHttpMinor(req) == 1 && Host.from(req.headers).isEmpty) { // need to add the host header for HTTP/1.1
       uri.host match {
         case Some(host) =>

blaze-client/src/test/scala/org/http4s/client/blaze/Http1ClientStageSpec.scala
@@ -32,10 +32,14 @@ class Http1ClientStageSpec extends Specification with NoTimeConversions {
   }
 
   "Http1ClientStage" should {
+
+    // Common throw away response
+    val resp = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\ndone"
+
     "Submit a request line with a query" in {
-      val resp = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\ndone"
-      val uri = "http://www.foo.com/huh?foo=bar"
-      val \/-(parsed) = Uri.fromString(uri)
+
+      val uri = "/huh?foo=bar"
+      val \/-(parsed) = Uri.fromString("http://www.foo.com" + uri)
       val req = Request(uri = parsed)
 
       val response = getSubmission(req, resp).split("\r\n")