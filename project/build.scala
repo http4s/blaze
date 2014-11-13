e1902a20f5a5c312ad22a0e68969b9b930b0088d
Bryce Anderson <bryce.anderson22@gmail.com>
2014-11-13 18:49:38 -0500

    Upgrade to http4s-websocket 0.1.1

--- project/build.scala
+++ project/build.scala
@@ -98,7 +98,7 @@ object ApplicationBuild extends Build {
   )
 
   lazy val specs2              = "org.specs2"                 %% "specs2"              % "2.4"
-  lazy val http4sWebsocket     = "org.http4s"                 %% "http4s-websocket"    % "0.1.0"
+  lazy val http4sWebsocket     = "org.http4s"                 %% "http4s-websocket"    % "0.1.1"
   lazy val scalaloggingSlf4j   = "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "2.1.2"
   lazy val logbackClassic      = "ch.qos.logback"             %  "logback-classic"     % "1.0.9"
   lazy val scalaXml = "org.scala-lang.modules"                %% "scala-xml"           % "1.0.2"
