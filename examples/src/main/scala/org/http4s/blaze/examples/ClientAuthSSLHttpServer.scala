b937f78eac5d5acd954aadce9b7824d10df31555
Bryce Anderson <bryce.anderson22@gmail.com>
2014-12-07 21:18:27 -0500

    Add example of SSL client authentication
    closes #5
    Seems to work fine. It should also work with blaze artifacts, but the SSLStage is less stable.

--- examples/src/main/scala/org/http4s/blaze/examples/ClientAuthSSLHttpServer.scala
+++ examples/src/main/scala/org/http4s/blaze/examples/ClientAuthSSLHttpServer.scala
@@ -1,21 +1,21 @@
 package org.http4s.blaze.examples
 
-import org.http4s.blaze.channel._
 import java.net.InetSocketAddress
 import java.nio.channels.AsynchronousChannelGroup
-import org.http4s.blaze.pipeline.stages.SSLStage
 
+import org.http4s.blaze.channel._
 import org.http4s.blaze.channel.nio2.NIO2SocketServerChannelFactory
 import org.http4s.blaze.pipeline.TrunkBuilder
+import org.http4s.blaze.pipeline.stages.SSLStage
 
+class ClientAuthSSLHttpServer(port: Int) {
 
-class SSLHttpServer(port: Int) {
-
-  private val sslContext = ExampleKeystore.sslContext()
+  private val sslContext = ExampleKeystore.clientAuthSslContext()
 
   private val f: BufferPipelineBuilder = { _ =>
     val eng = sslContext.createSSLEngine()
     eng.setUseClientMode(false)
+    eng.setNeedClientAuth(true)
 
     TrunkBuilder(new SSLStage(eng, 100*1024)).cap(new ExampleHttpServerStage(None, 10*1024))
   }
@@ -27,6 +27,6 @@ class SSLHttpServer(port: Int) {
   def run(): Unit = factory.bind(new InetSocketAddress(port)).run()
 }
 
-object SSLHttpServer {
-  def main(args: Array[String]): Unit = new SSLHttpServer(4430).run()
+object ClientAuthSSLHttpServer {
+  def main(args: Array[String]): Unit = new ClientAuthSSLHttpServer(4430).run()
 }
--- examples/src/main/scala/org/http4s/blaze/examples/ExampleKeystore.scala
+++ examples/src/main/scala/org/http4s/blaze/examples/ExampleKeystore.scala
@@ -1,7 +1,7 @@
 package org.http4s.blaze.examples
 
 import java.security.KeyStore
-import javax.net.ssl.{KeyManagerFactory, SSLContext}
+import javax.net.ssl.{TrustManagerFactory, KeyManagerFactory, SSLContext}
 
 import org.http4s.blaze.util.BogusKeystore
 
@@ -9,16 +9,48 @@ object ExampleKeystore {
 
   def sslContext(): SSLContext = {
     val ksStream = BogusKeystore.asInputStream()
+    assert(ksStream != null)
 
     val ks = KeyStore.getInstance("JKS")
     ks.load(ksStream, BogusKeystore.getKeyStorePassword)
+
 
     val kmf = KeyManagerFactory.getInstance("SunX509")
     kmf.init(ks, BogusKeystore.getCertificatePassword)
 
     val context = SSLContext.getInstance("SSL")
 
+
     context.init(kmf.getKeyManagers(), null, null)
+
+    context
+  }
+
+  def clientAuthSslContext(): SSLContext = {
+    /*
+      keys were generated using the script at
+      http://www.chesterproductions.net.nz/blogs/it/code/configuring-client-certificate-authentication-with-tomcat-and-java/537/
+      In order to access the server, you need to import the 'client.p12' file into your browsers certificate store.
+     */
+
+    val ksStream = getClass.getResourceAsStream("/clientauth/server.jks") //BogusKeystore.asInputStream()
+    assert(ksStream != null)
+
+    val ks = KeyStore.getInstance("JKS")
+    ks.load(ksStream, "password".toCharArray)
+
+
+    val kmf = KeyManagerFactory.getInstance("SunX509")
+    kmf.init(ks, "password".toCharArray)
+
+    val context = SSLContext.getInstance("SSL")
+
+    // the keystore is also used as the trust manager
+    val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+    tmf.init(ks)
+
+
+    context.init(kmf.getKeyManagers(), tmf.getTrustManagers, null)
 
     context
   }
