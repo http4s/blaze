ef0d751b5a4e20c80f3399850c18c635523e65dd
Bryce Anderson <bryce.anderson22@gmail.com>
2014-12-10 17:05:53 -0500

    0.4.0 release

--- project/build.scala
+++ project/build.scala
@@ -56,7 +56,7 @@ object ApplicationBuild extends Build {
   lazy val buildSettings = Defaults.defaultSettings ++ publishing ++ Seq(
     organization := "org.http4s",
 
-    version := "0.4.0-SNAPSHOT",
+    version := "0.4.0",
 
     scalaVersion := "2.11.4",
 
