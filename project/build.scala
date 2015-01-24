43c9de94b5ca05e9576971a00588dfc2281955b5
Bryce Anderson <bryce.anderson22@gmail.com>
2015-01-23 21:14:50 -0500

    Bump scala to 2.11.5

--- project/build.scala
+++ project/build.scala
@@ -61,7 +61,7 @@ object ApplicationBuild extends Build {
 
     scalaVersion := "2.11.5",
 
-    crossScalaVersions := Seq("2.10.4", "2.11.4"),
+    crossScalaVersions := Seq("2.10.4", "2.11.5"),
 
     description := "NIO Framework for Scala",
 
