cc3aa2f338127e49262573a4cc56784d34d4ae93
Bryce Anderson <bryce.anderson22@gmail.com>
2015-02-04 10:11:49 -0500

    Bump version to 0.5.1 for release

--- project/build.scala
+++ project/build.scala
@@ -57,7 +57,7 @@ object ApplicationBuild extends Build {
   lazy val buildSettings = Defaults.defaultSettings ++ publishing ++ Seq(
     organization := "org.http4s",
 
-    version := "0.5.1-SNAPSHOT",
+    version := "0.5.1",
 
     scalaVersion := "2.11.5",
 
