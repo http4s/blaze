7252be78cfa698168df0ccf396fd4c1fce4991aa
Bryce Anderson <bryce.anderson22@gmail.com>
2015-09-19 11:31:25 -0400

    Make noopCancellable public.
    Its useful sometimes.

--- core/src/main/scala/org/http4s/blaze/util/Cancellable.scala
+++ core/src/main/scala/org/http4s/blaze/util/Cancellable.scala
@@ -1,10 +1,7 @@
 package org.http4s.blaze.util
 
-/**
- * Created on 7/16/15.
- */
 object Cancellable {
-  private[blaze] val noopCancel = new Cancellable {
+  val noopCancel = new Cancellable {
     def cancel(): Unit = ()
   }
 }
