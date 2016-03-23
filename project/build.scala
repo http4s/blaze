fb4996c3ce6a60777dee22993ac283e06c877514
Ross A. Baker <ross@rossabaker.com>
2016-03-23 15:16:37 -0400

    Release v0.12.0.

--- project/build.scala
+++ project/build.scala
@@ -62,7 +62,7 @@ object ApplicationBuild extends Build {
   lazy val buildSettings = Defaults.defaultSettings ++ publishing ++ Seq(
     organization := "org.http4s",
 
-    version := "0.12.0-SNAPSHOT",
+    version := "0.12.0",
 
     scalaVersion := primaryScalaVerison,
 
