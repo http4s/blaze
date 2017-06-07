a28d775665a374b5dbb9aa9ca8791d91e1de9c8a
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-06-07 12:38:11 -0400

    Explicit Failure Messages

--- travis-scripts/deploy.sh
+++ travis-scripts/deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-sudo chmod +x /usr/local/bin/sbt # Temporary Fix For https://github.com/travis-ci/travis-ci/issues/7703
+if [ $TRAVIS_PULL_REQUEST != 'false' ]; then
+    echo "Pull Requests are not released"
 
-if [ $TRAVIS_PULL_REQUEST != 'false' ]
-then
-    echo "Only Repository Merged Code is Released"
+elif [ $TRAVIS_REPO_SLUG != "http4s/blaze" ]; then
+    echo "Only Http4s/Blaze Repository Code is Released"
 
-elif [ $TRAVIS_BRANCH != "master" ]
-then
+elif [ $TRAVIS_BRANCH != "master" ]; then
     echo "Only Master Branch is Released"
+
 else
+   sudo chmod +x /usr/local/bin/sbt # Temporary Fix For https://github.com/travis-ci/travis-ci/issues/7703
    sbt ++$TRAVIS_SCALA_VERSION 'release with-defaults'
+
 fi
 
