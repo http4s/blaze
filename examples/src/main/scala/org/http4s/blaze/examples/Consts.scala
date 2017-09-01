0bcb7e3fee4f53688cd0704209d546b6a094fa83
Bryce Anderson <bryce.anderson22@gmail.com>
2017-08-31 20:09:39 -0600

    cleanup examples

--- examples/src/main/scala/org/http4s/blaze/examples/Consts.scala
+++
@@ -1,8 +0,0 @@
-package org.http4s.blaze.examples
-
-object Consts {
-
-  /** Default thread pool size. */
-  val poolSize = (Runtime.getRuntime.availableProcessors()*1.5 + 1).toInt
-
-}
--- http/src/main/scala/org/http4s/blaze/http/HttpServerStageConfig.scala
+++ http/src/main/scala/org/http4s/blaze/http/HttpServerStageConfig.scala
@@ -1,7 +1,5 @@
 package org.http4s.blaze.http
 
-import java.net.InetSocketAddress
-
 import org.http4s.blaze.util.Execution
 
 import scala.concurrent.ExecutionContext
@@ -16,4 +14,5 @@ case class HttpServerStageConfig(
   maxNonBodyBytes: Int = 16*1024,                           // Max bytes to accept as part of headers and request line
   requestPreludeTimeout: Duration = Duration.Inf,           // Timeout for the next request before considering the session lost
   serviceTimeout: Duration = Duration.Inf,                  // Timeout to apply to the service before sending a
+  maxConcurrentStreams: Int = 100,                          // Maximum number of inbound streams we allow concurrently for HTTP2
   serviceExecutor: ExecutionContext = Execution.trampoline) // Executor to run the service future in.
