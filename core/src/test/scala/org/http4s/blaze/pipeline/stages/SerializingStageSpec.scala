332c1c5e361f7b93da19279a51e1c5c1080c2ffd
Bryce Anderson <bryce.anderson22@gmail.com>
2017-10-05 21:58:09 -0600

    Make SerializingStageSpec deterministic
    The tests previously made hundreds of threads which
    is really poor form.

--- core/src/test/scala/org/http4s/blaze/pipeline/stages/SerializingStageSpec.scala
+++ core/src/test/scala/org/http4s/blaze/pipeline/stages/SerializingStageSpec.scala
@@ -1,31 +1,20 @@
 package org.http4s.blaze.pipeline
 package stages
 
-
+import org.http4s.blaze.util.Execution
 import org.specs2.mutable._
-import java.util.concurrent.atomic.AtomicInteger
-import scala.collection.mutable.ListBuffer
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class SerializingStageSpec extends Specification {
 
-  class SlowIntHead extends SlowHead[Int] {
-
-    val ints = new ListBuffer[Int]
-
-    val i = new AtomicInteger(0)
-    def get: Int = i.getAndIncrement
-
-    def write(data: Int): Unit = {
-      ints += data
-    }
-
-    def name: String = "SlowIntHead"
-  }
+  // It's critical to use a thread-local execution context to ensure that
+  // race conditions aren't happening between the data feeding into and
+  // out of the SlowHead and whatever thread the continuations are being
+  // completed on.
+  private implicit val ec = Execution.trampoline
 
   class Nameless extends TailStage[Int] {
     def name: String = "int getter"
@@ -33,44 +22,54 @@ class SerializingStageSpec extends Specification {
 
   "SerializingStage" should {
 
-    val tail = new Nameless
-    val head = new SlowIntHead
-
-    // build our pipeline
-    LeafBuilder(tail).prepend(new SerializingStage[Int]).base(head)
-
     val ints = (0 until 200).toList
 
     "serialize reads" in {
       val tail = new Nameless
-      val head = new SlowIntHead
+      val head = new SlowHead[Int]
 
       // build our pipeline
       LeafBuilder(tail).prepend(new SerializingStage[Int]).base(head)
 
-
-      val results = ints map { i =>
+      val results = ints.map { _ =>
         tail.channelRead()
       }
 
       val numbers = Future.sequence(results)
+
+      ints.foreach { i =>
+        val p = head.takeRead
+        p.success(i)
+      }
+
       Await.result(numbers, 10.seconds) should_== ints
     }
 
     "serialize writes" in {
-      val f = (0 until 100).map{ i =>
+      val tail = new Nameless
+      val head = new SlowHead[Int]
+
+      // build our pipeline
+      LeafBuilder(tail).prepend(new SerializingStage[Int]).base(head)
+
+      val f = (0 until 100).map { i =>
         tail.channelWrite(i)
       }.last
 
-      val f2 = f.flatMap{ _ =>
-        (100 until 200 by 2).map{ j =>
+      val f2 = f.flatMap { _ =>
+        (100 until 200 by 2).map { j =>
           tail.channelWrite(List(j, j+1))
         }.last
       }
 
+      val writes = ints.map { _ =>
+        val write = head.takeWrite
+        write.completion.success(())
+        write.value
+      }
+
       Await.result(f2, 20.seconds)
-      head.ints.result() should_== ints
+      writes should_== ints
     }
   }
-
 }
