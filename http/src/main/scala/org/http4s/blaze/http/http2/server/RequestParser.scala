5fa5677097e1a1e8d745b3b1e5919282916d4570
Bryce Anderson <bryce.anderson22@gmail.com>
2018-02-06 20:47:52 -0700

    Add tests for RequestParser

--- http/src/main/scala/org/http4s/blaze/http/http2/server/RequestParser.scala
+++ http/src/main/scala/org/http4s/blaze/http/http2/server/RequestParser.scala
@@ -9,7 +9,7 @@ import scala.collection.mutable.ArrayBuffer
 private object RequestParser {
   def makeRequest(hs: Headers, body: BodyReader): Either[String, HttpRequest] = {
 
-    val normalHeaders = new ArrayBuffer[(String, String)](hs.size)
+    val normalHeaders = new ArrayBuffer[(String, String)](math.max(0, hs.size - 3))
     var method: String = null
     var scheme: String = null
     var path: String = null
@@ -17,48 +17,64 @@ private object RequestParser {
     var pseudoDone = false
 
     hs.foreach {
-      case (Method, v)    =>
-        if (pseudoDone) error += "Pseudo header in invalid position. "
-        else if (method == null) method = v
-        else error += "Multiple ':method' headers defined. "
+      case (k, v) if isPseudo(k) =>
+        if (pseudoDone) error += s"Pseudo header ($k) in invalid position. "
+        else k match {
+          case Method =>
+            if (method == null) method = v
+            else error += "Multiple ':method' headers defined. "
 
-      case (Scheme, v)    =>
-        if (pseudoDone) error += "Pseudo header in invalid position. "
-        else if (scheme == null) scheme = v
-        else error += "Multiple ':scheme' headers defined. "
+          case Scheme =>
+            if (scheme == null) scheme = v
+            else error += "Multiple ':scheme' headers defined. "
 
-      case (Path, v)      =>
-        if (pseudoDone) error += "Pseudo header in invalid position. "
-        else if (path == null)   {
-          if (v.isEmpty) error += "Received :path pseudo-header with empty value. "
-          path = v
+          case Path =>
+            if (path == null) {
+              if (v.isEmpty) error += "Received :path pseudo-header with empty value. "
+              path = v
+            }
+            else error += "Multiple ':path' headers defined. "
+
+          case Authority => // NOOP; TODO: maybe we should synthesize a Host header out of this?
+
+          case _ =>
+            // Endpoints MUST treat a request or response that contains
+            // undefined or invalid pseudo-header fields as malformed
+            // https://tools.ietf.org/html/rfc7540#section-8.1.2.1
+            error += s"Invalid pseudo header: $k. "
         }
-        else error += "Multiple ':path' headers defined. "
 
-      case (Authority, _) => // NOOP; TODO: maybe we should synthesize a Host header out of this?
-        if (pseudoDone) error += "Pseudo header in invalid position. "
-
-      case h@(k, _) if k.startsWith(":") => error += s"Invalid pseudo header: $h. "
-      case h@(k, _) if !validHeaderName(k) => error += s"Invalid header key: $k. "
-
-      case hs =>    // Non pseudo headers
+      case h@(k, v) => // Non pseudo headers
         pseudoDone = true
-        hs match {
-          case h@(Connection, _) => error += s"HTTP/2.0 forbids connection specific headers: $h. "
+        k match {
+          case Connection =>
+            error += s"HTTP/2.0 forbids connection specific headers: $h. "
 
-          case h@(TE, v) =>
+          case TE =>
             if (!v.equalsIgnoreCase("trailers")) error += s"HTTP/2.0 forbids TE header values other than 'trailers'. "
           // ignore otherwise
 
-          case header => normalHeaders += header
+          case _ if !validHeaderName(k) =>
+            error += s"Invalid header key: $k. "
+
+          case header =>
+            normalHeaders += h
         }
     }
 
+    // All HTTP/2 requests MUST include exactly one valid value for the
+    // ":method", ":scheme", and ":path" pseudo-header fields, unless it is
+    // a CONNECT request (Section 8.3).  An HTTP request that omits
+    // mandatory pseudo-header fields is malformed (Section 8.1.2.6).
+    // https://tools.ietf.org/html/rfc7540#section-8.1.2.3
     if (method == null || scheme == null || path == null) {
       error += s"Invalid request: missing pseudo headers. Method: $method, Scheme: $scheme, path: $path. "
     }
 
     if (0 < error.length) Left(error)
-    else Right(HttpRequest(method, path, 2, 0, hs, body))
+    else Right(HttpRequest(method, path, 2, 0, normalHeaders.result(), body))
   }
+
+  private[this] def isPseudo(key: String): Boolean =
+    0 < key.length && key.charAt(0) == ':'
 }
