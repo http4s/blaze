8bbea654b8e735a1db27555bcb0fcf7fe7a20214
Bryce Anderson <bryce.anderson22@gmail.com>
2018-02-09 20:49:44 -0700

    Add the HTTP/2 AbstractBodyWriter
    It serves as the BodyWriter implementation for HTTP/2 servers.

---
+++ http/src/test/scala/org/http4s/blaze/http/http2/server/AbstractBodyWriterSpec.scala
@@ -0,0 +1,127 @@
+package org.http4s.blaze.http.http2.server
+
+import java.io.IOException
+import java.nio.ByteBuffer
+
+import org.http4s.blaze.http.http2.{DataFrame, HeadersFrame, Priority, StreamMessage}
+import org.http4s.blaze.util.BufferTools
+import org.specs2.mutable.Specification
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+class AbstractBodyWriterSpec extends Specification {
+
+  private val hs = Seq("one" -> "two")
+
+  private def hsFrame(eos: Boolean): HeadersFrame = HeadersFrame(Priority.NoPriority, eos, hs)
+
+  private def data(size: Int): ByteBuffer = BufferTools.allocate(size)
+
+  private def dataFrame(eos: Boolean, size: Int): DataFrame = DataFrame(eos, data(size))
+
+  private def checkSuccess(t: Future[Unit]): Boolean = t.value match {
+    case Some(Success(())) => true
+    case other => sys.error(s"Unexpected value: $other")
+  }
+
+  private def checkIOException(t: Future[Unit]): Boolean = t.value match {
+    case Some(Failure(_: IOException)) => true // nop
+    case other => sys.error(s"Unexpected value: $other")
+  }
+
+  private class WriterImpl extends AbstractBodyWriter(hs) {
+
+    var flushed: Option[Seq[StreamMessage]] = None
+
+    override protected def flushMessage(msg: StreamMessage): Future[Unit] = flushMessage(msg::Nil)
+
+    override protected def flushMessage(msg: Seq[StreamMessage]): Future[Unit] = {
+      assert(flushed.isEmpty)
+      flushed = Some(msg)
+      Future.successful(())
+    }
+  }
+
+  "AbstractBodyWriter" >> {
+    "flush" >> {
+      "Flushes headers if they haven't been written" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.flush())
+        writer.flushed must_== Some(Seq(hsFrame(false)))
+      }
+
+      "Flushes nothing if the headers are already gone" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.flush())
+        writer.flushed must_== Some(Seq(hsFrame(false)))
+        writer.flushed = None
+
+        // try again
+        checkSuccess(writer.flush())
+        writer.flushed must beNone
+      }
+
+      "Flush results in failure of already closed" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.close())
+        checkIOException(writer.flush())
+      }
+    }
+
+    "write" >> {
+      "Flushes headers and data if they haven't been written" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.write(data(1)))
+        writer.flushed must_== Some(Seq(hsFrame(false), dataFrame(false, 1)))
+      }
+
+      "Flushes data if the headers are already gone" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.flush())
+        writer.flushed must beSome
+        writer.flushed = None
+
+        checkSuccess(writer.write(data(1)))
+        writer.flushed must_== Some(Seq(dataFrame(false, 1)))
+      }
+
+      "Flush results in failure of already closed" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.close())
+        writer.flushed must beSome
+        writer.flushed = None
+
+        checkIOException(writer.write(data(1)))
+      }
+    }
+
+    "close" >> {
+      "Flushes headers if they haven't been written" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.close())
+        writer.flushed must_== Some(Seq(hsFrame(true)))
+      }
+
+      "Flushes empty data if the headers are already gone" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.flush())
+        writer.flushed must_== Some(Seq(hsFrame(false)))
+        writer.flushed = None
+
+        checkSuccess(writer.close())
+        writer.flushed must_== Some(Seq(dataFrame(true, 0)))
+      }
+
+      "results in failure of already closed" >> {
+        val writer = new WriterImpl
+        checkSuccess(writer.close())
+        writer.flushed must_== Some(Seq(hsFrame(true)))
+        writer.flushed = None
+
+        checkIOException(writer.close())
+      }
+    }
+  }
+
+}
