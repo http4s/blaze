81eb946cd04797ccf7934e9b4882d77f11256e31
Scala steward <me@scala-steward.org>
2019-02-27 14:36:43 +0100

    Update async-http-client to 2.8.0

--- project/BlazePlugin.scala
+++ project/BlazePlugin.scala
@@ -72,7 +72,7 @@ object BlazePlugin extends AutoPlugin {
 
   lazy val logbackClassic      = "ch.qos.logback"             %  "logback-classic"     % "1.2.3"
   lazy val twitterHPACK        = "com.twitter"                %  "hpack"               % "1.0.2"
-  lazy val asyncHttpClient     = "org.asynchttpclient"        %  "async-http-client"   % "2.7.0"
+  lazy val asyncHttpClient     = "org.asynchttpclient"        %  "async-http-client"   % "2.8.0"
   lazy val log4s               = "org.log4s"                  %% "log4s"               % "1.7.0"
   lazy val scalacheck          = "org.scalacheck"             %% "scalacheck"          % "1.14.0"
   lazy val specs2              = "org.specs2"                 %% "specs2-core"         % "4.4.1"
