a2ae8860e547bf048ec676cfd58f922db61fe532
Scala Steward <me@scala-steward.org>
2019-10-02 00:41:32 +0200

    Update jetty-alpn-openjdk8-client to 9.4.21.v20190926

--- project/BlazePlugin.scala
+++ project/BlazePlugin.scala
@@ -72,6 +72,6 @@ object BlazePlugin extends AutoPlugin {
   lazy val alpn_api            = "org.eclipse.jetty.alpn"     % "alpn-api"             % "1.1.3.v20160715"
   // Note that the alpn_boot version is JVM version specific. Check the docs if getting weird errors.
   // Also note that only java8 and above has the require cipher suite for http2.
-  lazy val alpn_boot           = "org.eclipse.jetty"          % "jetty-alpn-openjdk8-client" % "9.4.20.v20190813"
+  lazy val alpn_boot           = "org.eclipse.jetty"          % "jetty-alpn-openjdk8-client" % "9.4.21.v20190926"
 
 }
