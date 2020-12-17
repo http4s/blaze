94b4f8a6e0e1aa654e15c604e1ce1707820f2a23
Scala Steward <me@scala-steward.org>
2020-12-17 16:37:18 +0100

    Update scalacheck to 1.15.2

--- project/BlazePlugin.scala
+++ project/BlazePlugin.scala
@@ -28,7 +28,7 @@ object BlazePlugin extends AutoPlugin {
   lazy val twitterHPACK        = "com.twitter"                %  "hpack"               % "1.0.2"
   lazy val asyncHttpClient     = "org.asynchttpclient"        %  "async-http-client"   % "2.12.1"
   lazy val log4s               = "org.log4s"                  %% "log4s"               % "1.10.0-M3"
-  lazy val scalacheck          = "org.scalacheck"             %% "scalacheck"          % "1.15.1"
+  lazy val scalacheck          = "org.scalacheck"             %% "scalacheck"          % "1.15.2"
   lazy val specs2              = "org.specs2"                 %% "specs2-core"         % "4.10.5"
   lazy val specs2Mock          = "org.specs2"                 %% "specs2-mock"         % specs2.revision
   lazy val specs2Scalacheck    = "org.specs2"                 %% "specs2-scalacheck"   % specs2.revision
