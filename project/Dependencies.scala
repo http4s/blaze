9e1835a01b6eda9aa2405e7be7722943ca14a160
Ross A. Baker <ross@rossabaker.com>
2021-01-30 23:41:18 -0500

    Downgrade to log4s-1.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val logbackClassic      = "ch.qos.logback"             %  "logback-classic"     % "1.2.3"
   lazy val twitterHPACK        = "com.twitter"                %  "hpack"               % "1.0.2"
   lazy val asyncHttpClient     = "org.asynchttpclient"        %  "async-http-client"   % "2.12.2"
-  lazy val log4s               = "org.log4s"                  %% "log4s"               % "1.10.0-M3"
+  lazy val log4s               = "org.log4s"                  %% "log4s"               % "1.9.0"
   lazy val scalacheck          = "org.scalacheck"             %% "scalacheck"          % "1.15.2"
   lazy val specs2              = "org.specs2"                 %% "specs2-core"         % "4.10.6"
   lazy val specs2Mock          = "org.specs2"                 %% "specs2-mock"         % specs2.revision
