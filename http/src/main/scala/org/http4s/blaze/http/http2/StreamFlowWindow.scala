8030b6fce3a33c99c725d62bb2819bb1e75cff90
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-01 23:11:17 -0400

    spelling: initial
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- http/src/main/scala/org/http4s/blaze/http/http2/StreamFlowWindow.scala
+++ http/src/main/scala/org/http4s/blaze/http/http2/StreamFlowWindow.scala
@@ -50,7 +50,7 @@ abstract class StreamFlowWindow {
     * If an error is returned, the internal state _must not_ be modified.
     *
     * @param delta
-    *   change in intial window size. Maybe be positive or negative, but must not cause the window
+    *   change in initial window size. Maybe be positive or negative, but must not cause the window
     *   to overflow Int.MaxValue.
     */
   def remoteSettingsInitialWindowChange(delta: Int): Option[Http2Exception]
