e6600611f9ee2aafd6947a456bf5e2c2296703dc
Ross A. Baker <ross@rossabaker.com>
2023-05-13 22:27:27 -0400

    Comment on fromFutureNoShift's uncancelability

--- blaze-core/src/main/scala/org/http4s/blazecore/util/package.scala
+++ blaze-core/src/main/scala/org/http4s/blazecore/util/package.scala
@@ -42,6 +42,8 @@ package object util extends ParasiticExecutionContextCompat {
         case Some(value) =>
           F.fromTry(value)
         case None =>
+          // Scala futures are uncancelable.  There's not much we can
+          // do here other than async_.
           F.async_ { cb =>
             future.onComplete {
               case Success(a) => cb(Right(a))
